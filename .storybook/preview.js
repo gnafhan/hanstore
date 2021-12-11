7b8f8acd9bf5490a974824dbdfa66bc0aa0f9497
jonisutanto <nafhanghifari@gmail.com>
2021-12-11 09:31:12 +0700

    Menambah Global CSS

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,21 @@
+import '../styles/utilities.css'
+import '../styles/checkout.css'
+import '../styles/complete-checkout.css'
+import '../styles/homepage.css'
+import '../styles/sign-in.css'
+import '../styles/detail.css'
+import '../styles/404-not-found.css'
+import '../styles/edit-profile.css'
+import '../styles/sign-up.css'
+import '../styles/sign-up-photo.css'
+import '../styles/sign-up-photo-success.css'
+import '../styles/sign-up-success.css'
+import '../styles/overview.css'
+import '../styles/transactions.css'
+import '../styles/transactions-detail.css'
+import '../styles/navbar-log-in.css'
+import '../styles/sidebar.css'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
