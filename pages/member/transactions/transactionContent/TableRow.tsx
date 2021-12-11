bc112796fb04ea3f595fc89f3dfc407053e2f265
jonisutanto <nafhanghifari@gmail.com>
2021-12-11 08:20:16 +0700

    Refactor Member Transaction Done

--- pages/member/transactions/transactionContent/TableRow.tsx
+++ pages/member/transactions/transactionContent/TableRow.tsx
@@ -1,5 +1,7 @@
+import classNames from "classnames";
 import Image from "next/image"
 import Link from "next/link"
+import cx from "classnames"
 
 interface TransactionContentProps {
     image: string;
@@ -8,9 +10,18 @@ interface TransactionContentProps {
     item: number;
     price: number;
     status: string;
+    
 }
 export default function TableRow(props: TransactionContentProps) {
     const{image,title, category, item, price, status } = props;
+    const classStatus = cx({
+        'float-start' : true ,
+        'icon-status': true,
+        'pending' : status === 'Pending',
+        'success' : status === 'Success',
+        'failed' : status === 'Failed'
+
+    })
     return (
         <><tr data-category="pending" className="align-middle">
             <th scope="row">
@@ -29,7 +40,7 @@ export default function TableRow(props: TransactionContentProps) {
             </td>
             <td>
                 <div>
-                    <span className="float-start icon-status pending"></span>
+                    <span className={classStatus}></span>
                     <p className="fw-medium text-start color-palette-1 m-0 position-relative">
                         {status}</p>
                 </div>
--- pages/member/transactions/transactionContent/index.tsx
+++ pages/member/transactions/transactionContent/index.tsx
@@ -1,3 +1,4 @@
+import ButtonTab from "./ButtonTab";
 import TableRow from "./TableRow";
 
 export default function TransactionContent() {
@@ -13,13 +14,10 @@ export default function TransactionContent() {
                 <div className="row mt-30 mb-20">
                     <div className="col-lg-12 col-12 main-content">
                         <div id="list_status_title">
-                            <a data-filter="*" href="/#" className="btn btn-status rounded-pill text-sm btn-active me-3">All
-                                Trx</a>
-                            <a data-filter="success" href="/#"
-                                className="btn btn-status rounded-pill text-sm me-3">Success</a>
-                            <a data-filter="pending" href="/#"
-                                className="btn btn-status rounded-pill text-sm me-3">Pending</a>
-                            <a data-filter="failed" href="/#" className="btn btn-status rounded-pill text-sm me-3">Failed</a>
+                            <ButtonTab title ={"All Trx"} active/>
+                            <ButtonTab title ={"Success"} active ={false}/>
+                            <ButtonTab title ={"Pending"} active = {false}/>
+                            <ButtonTab title ={"Failed"} active = {false}/>
                         </div>
                     </div>
                 </div>
